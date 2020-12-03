4c7a77bba2372278ba49cb0c790c5323ed6de8cd
Gilszon <gilszon@gmail.com>
2020-12-03 11:25:09 -0300

    Tabela para o registro dos pedidos

--- fullstacteletro_classic/bd/pedido.sql
+++ fullstacteletro_classic/bd/pedido.sql
@@ -1,18 +1,11 @@
-CREATE TABLE `pedido` (
-  `num_pedido` int NOT NULL,
-  `nome_cliente` varchar(200) COLLATE utf8_bin NOT NULL,
-  `endereco` varchar(200) COLLATE utf8_bin NOT NULL,
-  `telefone` varchar(10) COLLATE utf8_bin NOT NULL,
-  `nome_produto` varchar(255) COLLATE utf8_bin NOT NULL,
-  `valor_unitario` decimal(8,2) NOT NULL,
-  `quantidade` tinyint NOT NULL,
-  `valor_total` decimal(8,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-ALTER TABLE `pedido`
-  ADD PRIMARY KEY (`num_pedido`);
-  
-ALTER TABLE `pedido`
-  MODIFY `num_pedido` int NOT NULL AUTO_INCREMENT;
-
-  
+  CREATE TABLE `pedidos` (
+  `num_pedidos` INT NOT NULL AUTO_INCREMENT,
+  `nome_cliente` VARCHAR(200) NOT NULL,
+  `endereco` VARCHAR(200) NOT NULL,
+  `telefone` VARCHAR(15) NOT NULL,
+  `nome_produto` VARCHAR(45) NOT NULL,
+  `valor_unitario` DECIMAL(8,2) NOT NULL,
+  `quantidade` TINYINT NOT NULL,
+  `valor_total` DECIMAL(8,2) NOT NULL,
+  `pedidoscol` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`num_pedidos`))ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_bin;
