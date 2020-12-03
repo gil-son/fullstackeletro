bbd0d0a82d5e4174ae7f9cb81306fc3747bc923e
Gilszon <gilszon@gmail.com>
2020-12-03 11:57:54 -0300

    Versão Clássica: HTML, Bootstrap, JS, MySQL e PHP

--- fullstacteletro_classic/index.php
+++ fullstacteletro_classic/index.php
@@ -6,7 +6,7 @@
     <title>Full Stack Eletro</title>
     <link rel="stylesheet" type="text/css" href="css/estilo.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-</head>
+</head> 
 <body>
     <!-- íncio Menu -->
         <?php include_once('menu.html') ?> 
