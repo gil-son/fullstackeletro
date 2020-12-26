953aa3040f77d9cbf5b3f9430c03582133e6cab8
gil-son <gilszon@gmail.com>
2020-12-26 19:26:25 -0300

    MVC - ReactJS com foco em Classes + PHP e MySQL

--- fullstackeletro_react_class_php/project_reactjs/src/componentes/Produto/index.js
+++ fullstackeletro_react_class_php/project_reactjs/src/componentes/Produto/index.js
@@ -46,54 +46,22 @@ export const ExibirProdutos = () => {
 
     
         return (
-            <section className="produtos">
-                <button onClick={ () => setCategoria("fogao")}>Fogão</button>
-                <div className="card-group">
-                  {produtos.map( //this.props.arrayProdutos.map
-                    row=>
+            <>
 
-                         { if(row.Categoria === categoria || categoria === "todos"){
-                            
-                        return(
-                            <div key={row.Id_Produto} className={`card card-produtos ${row.Categoria}`} onMouseOver={Selecionar}  onMouseOut={Desselecionar}>   
-                                <img className="card-img-top cartao m-auto pt-1 " src={`./img/${row.Imagem}`}  alt={`Card ${row.Categoria}`}  />
-                                <div className="card-body">
-                                    <p className="card-text text-center descricao"><small>{row.Descricao}</small></p>
-                                    <p className="card-text text-center descricao"><small><strike>{row.Preco}</strike></small></p>
-                                    <p className="card-text text-center preco"><small>{row.Preco_Final}</small></p>
-                                </div>
-                            </div>
-                         );
-                        }
-                      }
-                    )}
-                    
-                </div> 
-                
-            </section>
-
+            </>
         );
      
 }
 
 export const ExibirCategorias = () => {
 
-    // function fazeraponte(a){
-    //     ponte(a);
-    // }
-   
 
     return(
                 <section className="categorias">                
                     <h3 className="ml-2">Categorias</h3>
-                    <ul className="list-group shadow-lg">
-                      <li className="list-group-item list-group-item-action" >Todos (12)</li>
-                      <li className="list-group-item list-group-item-action" >Geladeira (3)</li>
-                      {/* <li className="list-group-item list-group-item-action" onClick={() => teste()}>Fogões (2)</li> */}
-                      <li className="list-group-item list-group-item-action">Microondas (3)</li>                     
-                      <li className="list-group-item list-group-item-action" onClick="exibirCategoria('lavadoura');">Lavadoura de roupas (2)</li>                     
-                      <li className="list-group-item list-group-item-action" onClick="exibirCategoria('lavaLoucas');">Lava louças (2)</li>                     
-                  </ul>
+                    
+
+
                 </section>
          );
 }
--- fullstackeletro_react_class_php/project_reactjs/src/pages/Produtos/index.jsx
+++ fullstackeletro_react_class_php/project_reactjs/src/pages/Produtos/index.jsx
@@ -11,14 +11,47 @@ import './style.css';
 
 
 
-class Produtos extends React.Component {
+const Produtos = () =>{
+
+  const[produtos, setProdutos] = React.useState([]);
+  let[categoria, setCategoria] = React.useState("todos");
+
+
+  React.useEffect(async () => { // obter a api de forma assincrona. No entando haverá esperas em sequência, quando necessário
+      const resposta = await fetch("http://localhost/Recode%20Pro/Atividade/fullstackeletro_react_class_php/model/Produto_to_JSON.php");
+      const db = await resposta.json();
+      setProdutos(db);
+  }, [categoria]); // Toda vez que categoria mudar, ele renderiza
 
 
 
+    function Selecionar(event){
+      //   console.log(event);
+           console.log(event.target); // Retorna aquele elemento do evento, com o tamanho 1
+      //   console.log(event.target.currentSrc);
+       
+         const get = event.currentTarget;
+         console.log(get);
+
+          const elemento = get.getElementsByClassName("cartao")[0];  // obtendo o único índice
+          console.log(elemento);
+
+          elemento.style.width = "25vh";
+          elemento.style.transition = "width 1s";
+          elemento.style.cursor = "zoom-in";
+    }
+
+ 
+
+      function Desselecionar(event){
+          const target = event.currentTarget;
+          const elemento = target.getElementsByClassName("cartao")[0];
+          elemento.style.width = "20vh";
+   }
 
 
   
-  render() {
+  
     return (
       <>
         
@@ -33,12 +66,47 @@ class Produtos extends React.Component {
               <div className="col-sm-12 col-md-3">
               
               <ExibirCategorias/>
+                  <ul className="list-group shadow-lg">
+                      <li className="list-group-item list-group-item-action" onClick={() => setCategoria("todos")}>Todos (12)</li>
+                      <li className="list-group-item list-group-item-action" onClick={() => setCategoria("geladeira")}>Geladeira (3)</li>
+                      <li className="list-group-item list-group-item-action" onClick={() => setCategoria("fogao")}>Fogões (2)</li>
+                      <li className="list-group-item list-group-item-action" onClick={() => setCategoria("microondas")}>Microondas (3)</li>                     
+                      <li className="list-group-item list-group-item-action" onClick={() => setCategoria("lavadoura")}>Lavadoura de roupas (2)</li>                     
+                      <li className="list-group-item list-group-item-action" onClick={() => setCategoria("lavaLoucas")}>Lava louças (2)</li>                     
+                  </ul>
 
               </div>
 
               <div className="col-sm-12 col-md-9">
                 
                 <ExibirProdutos/>
+                <section className="produtos">
+                <div className="card-group">
+                  
+                  {produtos.map( //this.props.arrayProdutos.map
+                    row=>
+
+                        { if(row.Categoria === categoria || categoria === "todos"){
+                            
+                        return(
+                            <div key={row.Id_Produto} className={`card card-produtos ${row.Categoria}`} onMouseOver={Selecionar}  onMouseOut={Desselecionar}>   
+                                <img className="card-img-top cartao m-auto pt-1 " src={`./img/${row.Imagem}`}  alt={`Card ${row.Categoria}`}  />
+                                <div className="card-body">
+                                    <p className="card-text text-center descricao"><small>{row.Descricao}</small></p>
+                                    <p className="card-text text-center descricao"><small><strike>{row.Preco}</strike></small></p>
+                                    <p className="card-text text-center preco"><small>{row.Preco_Final}</small></p>
+                                </div>
+                            </div>
+                         );
+                        }
+                      }
+                    )}                    
+                </div>                
+            </section>
+
+
+
+
 
               </div>
             </div>
@@ -48,7 +116,7 @@ class Produtos extends React.Component {
         
       </>
     );
-  }
+  
 }
 
 export default Produtos; 
