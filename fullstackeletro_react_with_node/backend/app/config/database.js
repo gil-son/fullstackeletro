669f827faf7fba4177b5eda8163cd116c111798d
Noslig <gilszon@gmail.com>
2021-01-29 00:25:42 -0300

    database

--- fullstackeletro_react_with_node/backend/app/config/database.js
+++ fullstackeletro_react_with_node/backend/app/config/database.js
@@ -1,7 +1,7 @@
 // Modules here
 var mysql = require('mysql');
 
-module.exports = function(){
+module.exports = function(){ 
     return mysql.createConnection({
         host:'localhost',
         user:'root',
