e2d9606d5bfc059e72256c894c0e6861245ac2f3
Noslig <gilszon@gmail.com>
2021-02-09 18:42:24 -0300

    Node

---
+++ fullstackeletro_x_react_native/backend/app/routes/tofront/comment.js
@@ -0,0 +1,33 @@
+// Modules here 
+
+// Config
+
+// Modules from
+var db = require('../../config/database.js');
+
+
+module.exports =  function(app){
+    var connection = db();
+
+    app.get("/comment", function(req, res){
+        var query = connection.query("SELECT * FROM comentario",
+        (error, result) => {
+    
+            // console.log("Dados:");
+            // console.log(result);
+            console.log(error, result);
+
+            if(error){
+                res.send(error);
+            }else{
+                res.send(result);
+            }
+            
+        });
+
+        query.on('error', function(error){
+            console.log("[mysql] error:", error);
+        });
+
+    });
+}
---
+++ fullstackeletro_x_react_native/backend/app/routes/tofront/product.js
@@ -0,0 +1,36 @@
+// Modules here 
+
+// Config
+
+// Modules from
+var db = require('../../config/database.js');
+
+
+module.exports =  function(app){
+    var connection = db();
+
+    app.get("/product", function(req, res){
+        var query = connection.query("SELECT * FROM produto",
+        (error, result) => {
+    
+            // console.log("Dados:");
+            // console.log(result);
+            console.log(error, result);
+
+            if(error){
+                res.send(error);
+            }else{
+                res.send(result);
+            }
+            
+        });
+
+        query.on('error', function(error){
+            console.log("[mysql] error:", error);
+        });
+
+    });
+}
+   
+
+
